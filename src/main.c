a453272aa7a802fb01aea84d544e50e16b483b58
Quentin Loddo <quentin.loddo@epitech.eu>
2022-03-29 11:53:29 +0200

    test

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** test_mirroring
+** File description:
+** main
+*/
+
+int main(int ac, char **av)
+{
+    int i = 0;
+
+
+    for (int j = 12; i < j; i += 1){
+        printf("%i\n", i);
+    }
+    return 0;
+}
